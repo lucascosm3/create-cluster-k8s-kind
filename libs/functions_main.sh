b9227549251d75f0b2921b97415904e28505123b
lucascosm3 <lcds1944@gmail.com>
2024-05-12 16:51:06 -0300

    fix: removing white space in post against slash

--- libs/functions_main.sh
+++ libs/functions_main.sh
@@ -1,5 +1,11 @@
-function _create_cluster (){
-kind create cluster \ 
-  --config config/config.yaml \ 
-  --name $CLUSTER_NAME
+function _create_cluster () {
+  kind create cluster \
+    --config config/config.yaml \
+    --name "$CLUSTER_NAME"
+
+  [ $ENABLE_INGRESS -eq 1 ] && _deploy_ingress
+}
+
+function _deploy_ingress () {
+  kubectl apply -f config/nginx/ingress.yaml
 }
